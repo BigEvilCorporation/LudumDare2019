3aa64d91061f7dd5b6665410a5bba9595fc814c8
mattphillips <mattphillips@mail.com>
2019-10-06 20:42:29 +0100

    SFX initial commit, working strain+pop on spawn

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,9 +23,15 @@ public class Player : MonoBehaviour
     public float DeadZoneFire = 0.1f;
     public float Gravity = 9.8f;
     public float FireTime = 0.5f;
+    public float StartTime = 2.0f;
     public GameObject BulletPrefab;
     public EnemySpawner EnemySpawner;
     public EvolutionStage[] EvolutionStages;
+
+    public AudioClip[] SFX_Strain;
+    public AudioClip[] SFX_Pop;
+    public AudioClip[] SFX_Suck;
+    public AudioClip[] SFX_Spit;
 
     public Vector3 Velocity
     {
@@ -55,16 +61,25 @@ public class Player : MonoBehaviour
     private SpriteRenderer m_sprite;
     private CharacterController m_characterController;
     private Sucker m_sucker;
+    private AudioSource m_audio;
     private int m_currentStageIdx;
     private int m_currentStageEnergy;
+    private float m_startTimer;
 
     void Start()
     {
         m_sprite = GetComponentInChildren<SpriteRenderer>();
         m_characterController = GetComponent<CharacterController>();
         m_sucker = GetComponentInChildren<Sucker>();
+        m_audio = GetComponent<AudioSource>();
         m_initialMoveSpeed = MoveSpeed;
         SetEvolutionStage(0);
+
+        //Start invisible, play strain SFX
+        m_sprite.enabled = false;
+        m_startTimer = StartTime;
+        m_audio.clip = SFX_Strain[(int)Random.Range(0, SFX_Strain.Length)];
+        m_audio.Play();
     }
 
     void SetEvolutionStage(int index)
@@ -102,67 +117,80 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        //Grab input
-        Vector2 stickInputLeft = new Vector2(Input.GetAxis("MoveX"), -Input.GetAxis("MoveY"));
-        Vector2 stickInputRight = new Vector2(Input.GetAxisRaw("RotateX"), Input.GetAxisRaw("RotateY"));
-
-        //Apply deadzone
-        if (Mathf.Abs(stickInputLeft.x) < DeadZoneLeft.x)
-            stickInputLeft.x = 0.0f;
-        if (Mathf.Abs(stickInputLeft.y) < DeadZoneLeft.y)
-            stickInputLeft.y = 0.0f;
-
-        m_stickInputLeft = stickInputLeft;
-        m_stickInputRight = stickInputRight;
-
-        //Apply velocity (scaled by current evolution)
-        m_velocity += new Vector3(m_stickInputLeft.x * CurrentEvolution.MoveSpeedScale, -Gravity * Time.deltaTime, m_stickInputLeft.y * CurrentEvolution.MoveSpeedScale);
-
-        //Apply drag
-        m_velocity.x /= 1.0f + Drag.x * Time.deltaTime;
-        m_velocity.z /= 1.0f + Drag.y * Time.deltaTime;
-
-        m_characterController.Move(Velocity * Time.deltaTime);
-
-        //Zero Y velocity if on floor
-        if ((m_characterController.collisionFlags & CollisionFlags.Below) != 0)
+        if (m_startTimer > 0.0f)
         {
-            m_velocity.y = 0.0f;
-        }
-
-        //Calc rotation direction
-        if (m_stickInputRight.SqrMagnitude() > DeadZoneRight)
-        {
-            Vector2 directionVector = m_stickInputRight.normalized;
-            Quaternion directionQuat = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), new Vector3(-directionVector.x, 0.0f, directionVector.y));
-
-            //Apply rotation
-            transform.rotation = directionQuat;
-        }
-
-        //Fire spitballs
-        m_fireTimer -= Time.deltaTime;
-
-        if (m_fireTimer <= 0.0f && Input.GetAxis("Fire1") > DeadZoneFire)
-        {
-            m_fireTimer = FireTime;
-
-            if (BulletPrefab)
+            m_startTimer -= Time.deltaTime;
+            if (m_startTimer <= 0.0f)
             {
-                GameObject spitballObj = Instantiate(BulletPrefab, transform.position, transform.rotation) as GameObject;
-                Spitball spitball = spitballObj.GetComponent<Spitball>();
-                spitball.Damage = CurrentEvolution.AttackDamage;
+                m_audio.clip = SFX_Pop[(int)Random.Range(0, SFX_Pop.Length)];
+                m_audio.Play();
+                m_sprite.enabled = true;
             }
-        }
-
-        //Suck up goo
-        if (Input.GetAxis("Fire2") > DeadZoneFire)
-        {
-            m_sucker.StartSuck();
         }
         else
         {
-            m_sucker.EndSuck();
+            //Grab input
+            Vector2 stickInputLeft = new Vector2(Input.GetAxis("MoveX"), -Input.GetAxis("MoveY"));
+            Vector2 stickInputRight = new Vector2(Input.GetAxisRaw("RotateX"), Input.GetAxisRaw("RotateY"));
+
+            //Apply deadzone
+            if (Mathf.Abs(stickInputLeft.x) < DeadZoneLeft.x)
+                stickInputLeft.x = 0.0f;
+            if (Mathf.Abs(stickInputLeft.y) < DeadZoneLeft.y)
+                stickInputLeft.y = 0.0f;
+
+            m_stickInputLeft = stickInputLeft;
+            m_stickInputRight = stickInputRight;
+
+            //Apply velocity (scaled by current evolution)
+            m_velocity += new Vector3(m_stickInputLeft.x * CurrentEvolution.MoveSpeedScale, -Gravity * Time.deltaTime, m_stickInputLeft.y * CurrentEvolution.MoveSpeedScale);
+
+            //Apply drag
+            m_velocity.x /= 1.0f + Drag.x * Time.deltaTime;
+            m_velocity.z /= 1.0f + Drag.y * Time.deltaTime;
+
+            m_characterController.Move(Velocity * Time.deltaTime);
+
+            //Zero Y velocity if on floor
+            if ((m_characterController.collisionFlags & CollisionFlags.Below) != 0)
+            {
+                m_velocity.y = 0.0f;
+            }
+
+            //Calc rotation direction
+            if (m_stickInputRight.SqrMagnitude() > DeadZoneRight)
+            {
+                Vector2 directionVector = m_stickInputRight.normalized;
+                Quaternion directionQuat = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), new Vector3(-directionVector.x, 0.0f, directionVector.y));
+
+                //Apply rotation
+                transform.rotation = directionQuat;
+            }
+
+            //Fire spitballs
+            m_fireTimer -= Time.deltaTime;
+
+            if (m_fireTimer <= 0.0f && Input.GetAxis("Fire1") > DeadZoneFire)
+            {
+                m_fireTimer = FireTime;
+
+                if (BulletPrefab)
+                {
+                    GameObject spitballObj = Instantiate(BulletPrefab, transform.position, transform.rotation) as GameObject;
+                    Spitball spitball = spitballObj.GetComponent<Spitball>();
+                    spitball.Damage = CurrentEvolution.AttackDamage;
+                }
+            }
+
+            //Suck up goo
+            if (Input.GetAxis("Fire2") > DeadZoneFire)
+            {
+                m_sucker.StartSuck();
+            }
+            else
+            {
+                m_sucker.EndSuck();
+            }
         }
     }
 
