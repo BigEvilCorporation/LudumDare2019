4985b8559b6d05a0d38ca6f0394c7cd43e021e0e
mattphillips <mattphillips@mail.com>
2019-10-06 13:31:03 +0100

    Camera zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,19 @@ public class CameraController : MonoBehaviour
     public float BoomHeight = 10.0f;
     public float BoomDistance = 20.0f;
     public float BoomAngle = 30.0f;
+    public float ZoomTime = 2.0f;
+
+    private Camera m_camera;
+    float m_initialSize;
+    float m_zoomTime = 1.0f;
+    float m_zoomStart = 1.0f;
+    float m_zoomTarget = 1.0f;
+
+    void Start()
+    {
+        m_camera = GetComponent<Camera>();
+        m_initialSize = m_camera.orthographicSize;
+    }
 
     void Update()
     {
@@ -16,6 +29,20 @@ public class CameraController : MonoBehaviour
             Vector3 position = new Vector3(TargetPlayer.transform.position.x, TargetPlayer.transform.position.y + BoomHeight, TargetPlayer.transform.position.z - BoomDistance);
             transform.position = position;
             transform.rotation = Quaternion.AngleAxis(BoomAngle, Vector3.right);
+
+            //If evolution scale changed
+            float cameraZoom = m_initialSize * TargetPlayer.CurrentEvolution.ScaleMax;
+            if(cameraZoom != m_zoomTarget)
+            {
+                //Start new zoom lerp
+                m_zoomStart = m_zoomTarget;
+                m_zoomTarget = cameraZoom;
+                m_zoomTime = 0.0f;
+            }
+
+            //Lerp camera
+            m_zoomTime = Mathf.Clamp(m_zoomTime + Time.deltaTime, 0.0f, 1.0f);
+            m_camera.orthographicSize = Mathf.SmoothStep(m_zoomStart, m_zoomTarget, m_zoomTime);
         }
     }
 }
