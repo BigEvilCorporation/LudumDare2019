1147ac9284c40d4b2d6a9a9e03806b6cefbb0981
mattphillips <mattphillips@mail.com>
2019-10-07 20:31:40 +0100

    Added sounds to intro

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -8,6 +8,9 @@ public class IntroManager : MonoBehaviour
 {
     public GameObject m_image02;
     public GameObject m_image03;
+    public AudioClip Sound1;
+    public AudioClip Sound2;
+    public AudioClip Sound3;
 
     float m_timer = 0f;
 
@@ -15,10 +18,14 @@ public class IntroManager : MonoBehaviour
     bool isActive02 = false;
     bool isChangingScene = false;
 
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = Sound1;
+        audioSource.Play();
     }
 
     // Update is called once per frame
@@ -30,6 +37,8 @@ public class IntroManager : MonoBehaviour
         {
             isActive01 = true;
             m_image02.SetActive(true);
+            audioSource.clip = Sound2;
+            audioSource.Play();
 
             return;
         }
@@ -38,6 +47,8 @@ public class IntroManager : MonoBehaviour
         {
             isActive02 = true;
             m_image03.SetActive(true);
+            audioSource.clip = Sound3;
+            audioSource.Play();
 
             return;
         }
